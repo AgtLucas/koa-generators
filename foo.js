be8c8308d05b67562ba773992d9703d3c01d5a9c
Lucas <lucas@agtlucas.com>
2014-10-26 11:22:14 -0200

    Update foo.js

--- foo.js
+++ foo.js
@@ -1,7 +1,21 @@
+/**
 function *foo (arg) {
   return arg;
 }
 
 var bar = foo(123);
 
+console.log(bar.next()); */
+
+function *foo () {
+  var index = 0;
+  while (index < 2) {
+    yield index++;
+  }
+}
+
+var bar = foo();
+
+console.log(bar.next());
+console.log(bar.next());
 console.log(bar.next());
