a4896b8c625f822814c77d5a1d0b15d223248d9e
Lucas <lucas@agtlucas.com>
2014-10-28 08:30:23 -0200

    Update loop.js

--- loop.js
+++ loop.js
@@ -1,7 +1,21 @@
-function* foo() {
-  yield 1;
-  yield 2;
-  yield 3;
+// function* foo() {
+//   yield 1;
+//   yield 2;
+//   yield 3;
+// }
+
+// for (v of foo()) {
+//   console.log(v);
+// }
+
+function *bar () {
+  yield 'b';
+}
+
+function *foo () {
+  yield 'a';
+  yield *bar();
+  yield 'c';
 }
 
 for (v of foo()) {
